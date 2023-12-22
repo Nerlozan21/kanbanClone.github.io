a1e33696359bcbc8c0cccc808202b858375716a3
Nerlozan <59479702+Nerlozan21@users.noreply.github.com>
2023-12-22 14:01:14 +0530

    Basic header structure

---
+++ src/Header.tsx
@@ -0,0 +1,23 @@
+import "./Header.css";
+import sunMoonIcon from "./assets/sun-moon.svg";
+import downArrow from "./assets/noun-dropdown-1542639.svg";
+import menu from "./assets/noun-toggle-menu-234402.svg";
+
+function Header() {
+  return (
+    <div id="main_container">
+      <div id="button_container">
+        <button id="display">
+          <img id="menu" src={menu} />
+          <span id='text'>Display</span>
+          <img id="down_arrow" src={downArrow} />
+        </button>
+      </div>
+      <button id="toggle_theme">
+        <img src={sunMoonIcon} alt="toggle_dark_mode" />
+      </button>
+    </div>
+  );
+}
+
+export default Header;
